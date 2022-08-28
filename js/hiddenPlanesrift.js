14d335d915806149e4a061d0fc1d89d66cd22db8
fishnoggin <masonblck@gmail.com>
2022-08-28 16:20:42 -0400

    fixed display type on hidden information

--- js/hiddenPlanesrift.js
+++ js/hiddenPlanesrift.js
@@ -21,15 +21,22 @@ function updateVisibility(){
 	hid = document.getElementsByClassName("hidden");
 	passkey = localStorage.planesriftPasskey;
 	for (var i = hid.length - 1; i >= 0; i--) {
+		var tagType = hid[i].tagName.toLowerCase();
+		var displayType;
+		if (tagType == 'h1' || tagType == 'h2' || tagType == 'h3' || tagType == 'h4') {
+			displayType = 'flex';
+		}else{
+			displayType = 'inherit';
+		}
 		if (passkey == "shalogorot") {
-			hid[i].style.display = 'inherit';
+			hid[i].style.display = displayType;
 		}
 		if (passkey == "mageslayer" && hid[i].classList.contains("azmira")) {
-			hid[i].style.display = 'inherit';
+			hid[i].style.display = displayType;
 		}
 
 		if (passkey == "Leroy Jankins" && hid[i].classList.contains("marwen")) {
-			hid[i].style.display = 'inherit';
+			hid[i].style.display = displayType;
 		}
 	}
 }
